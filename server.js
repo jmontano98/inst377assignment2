c3b1d277cc477aa61bf15dc61ed5647c91d6c63e
jmontano98 <lilcooljeff123@gmail.com>
2020-11-03 11:00:13 -0500

    Updated package and serverjs

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import countries from './public/lab_6/countries.js';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,12 +15,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+    res.send(`Lab 5 for ${process.env.NAME}`);
   })
-  .post((req, res) => {
+  .post(async (req, res) => {
+
+    const pgData = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const jsonObj = await pgData.json();
+
     console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    console.log(pgData);
+    console.log(jsonObj);
+    res.json(jsonObj);
+    //res.json(countries);
   });
 
 app.listen(port, () => {
